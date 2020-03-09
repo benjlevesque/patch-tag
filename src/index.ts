7599b6a6c2df0455da0d731e3b13225420171f67
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2020-03-09 18:19:24 +0100

    handle tag only

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import { execSync } from "child_process";
-import { getInput } from "@actions/core";
+import { getInput, info } from "@actions/core";
 
-const newTag = getInput("tag", {
-  required: true
-});
-const target = getInput("target", {
-  required: true
-});
-const path = getInput("path", {
-  required: true
-});
+export const getNewValue = (previousValue: string, newTag: string) => {
+  if (previousValue.match(/^[a-f0-9]*$/)) {
+    return newTag;
+  }
+  const result = /(?<image>.*):(?<tag>[a-f0-9]*)/.exec(previousValue);
+  if (!result) {
+    throw new Error(`cannot parse image ${previousValue}`);
+  }
+  return `${result.groups.image}:${newTag}`;
+};
 
-const oldImage = execSync(`yq r ${target} ${path}`).toString();
-const result = /(?<image>.*):(?<tag>[a-f0-9]{7})/.exec(oldImage);
-const newImage = `${result.groups.image}:${newTag}`;
-console.log(newImage);
-execSync(`yq w -i ${target} ${path} ${newImage}`);
+export const run = () => {
+  const newTag = getInput("tag", {
+    required: true
+  });
+  const target = getInput("target", {
+    required: true
+  });
+  const path = getInput("path", {
+    required: true
+  });
+
+  const oldImage = execSync(`yq r ${target} ${path}`).toString();
+  const newImage = getNewValue(oldImage, newTag);
+  info(newImage);
+  execSync(`yq w -i ${target} ${path} ${newImage}`);
+};
